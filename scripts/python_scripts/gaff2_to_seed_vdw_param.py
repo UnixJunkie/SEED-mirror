decccaa789ee2e22156ae74bc1cc3821adf036df
Cassiano Langini <cassiano.langini@gmail.com>
2022-01-14 19:24:48 +0100

    working for GAFF

---
+++ scripts/python_scripts/gaff2_to_seed_vdw_param.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+from __future__ import print_function
+import sys, os
+
+ATOM_MASS = {"C": 12, "H":1, "F":19, "CL":35, 
+             "BR": 80, "I":127, "N":14,"O":16, 
+             "P":31,"S":32,"CU":63, "CU2": 64,
+             "CA":40, "MG":24, "FE": 55, "ZN": 65, "EP": 0}
+ATOM_NUMBER = {"C": 6, "H": 1, "F": 9, "CL": 17,
+               "BR": 35, "I": 53, "N": 7, "O": 8,
+               "P": 15, "S": 16, "CU": 29, "CU2": 29,
+               "CA":20, "MG":12, "FE": 26, "ZN": 30, "EP": 0}
+
+def search_dict(my_dict, elem):
+    for k, v in my_dict.items():
+        if v == elem:
+            return(k)
+
+def main(argv):
+    parin = argv[0]
+    par_offset = int(argv[1])
+
+    with open(parin, "r") as f:
+        pars = f.readlines()
+
+    atmtype = []
+    atmrepr = {}
+    atmnum = {}
+    vdw_R = {}
+    vdw_eps = {}
+
+    out = ""
+    
+    inpars = True
+    invdw = False
+
+    for iline, line in enumerate(pars[1:]):
+        
+        # if line.startswith("AMBER"):
+        #     inpars = True 
+        #     continue
+
+        if line.startswith("MOD4"):
+            vdw_line = iline
+            invdw = True 
+            continue 
+
+        if inpars and len(line.split()) == 0:
+            inpars = False
+            continue
+
+        if invdw and len(line.split()) == 0:
+            invdw = False 
+            continue
+
+        if inpars:
+            ls = line.split()
+            atmtype.append(ls[0])
+            mass = round(float(ls[1]))
+            elem = search_dict(ATOM_MASS, mass) # get the element from the mass
+            # print(ls[0], mass, elem)
+            atmnum[ls[0]] = ATOM_NUMBER[elem] # stores the atom numbers for the atomtype
+            atmrepr[ls[0]] = line.rstrip("\n")[37:] #" ".join(ls[3:])
+            continue
+
+        if invdw:
+            ls = line.split()
+            vdw_R[ls[0]] = float(ls[1])
+            vdw_eps[ls[0]] = float(ls[2])
+
+    equivalences = {}
+
+    iline = vdw_line-1
+    # print(vdw_line-1)
+    while iline > 0:
+        line = pars[iline]
+        ls = line.split()
+        
+        if len(ls) == 0:
+            break
+        else:
+            equivalences[ls[0]] = ls[1:]
+
+        iline -= 1
+    
+    for k, v in equivalences.items():
+        for k1 in v:
+            vdw_R[k1] = vdw_R[k]
+            vdw_eps[k1] = vdw_eps[k]
+
+    # for k in atmtype:
+    #     try:
+    #         print(k, atmnum[k], vdw_R[k], vdw_eps[k], atmrepr[k])
+    #     except KeyError:
+    #         print("ERROR, missing key!")
+
+    counter = 0 + par_offset
+    for i, k in enumerate(atmtype):
+        try:
+            print(counter, k, atmnum[k], vdw_R[k], vdw_eps[k], "# "+atmrepr[k], sep="\t")
+            counter += 1
+        except KeyError:
+            print("# MISSING KEY ", k)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ scripts/python_scripts/mol2ori_to_mol2seed4_gaff.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+"""Clangini 14-01-2022
+"""
+from __future__ import print_function
+import sys, os
+
+def get_gaff_params(data):
+
+    charges = []
+    atmtypes = []
+
+    in_atom = False
+    in_mol = False
+    count = 0
+    for line in data:
+        if "@<TRIPOS>MOLECULE" in line:
+            in_mol = True
+            count += 1
+            continue
+
+        if "@<TRIPOS>ATOM" in line:
+            in_atom = True
+            in_mol = False
+            continue
+
+        if "@<TRIPOS>BOND" in line:
+            in_atom = False
+            break
+
+        if in_mol:
+            count += 1
+            if count == 3:
+                natom = int(line.split()[0])
+            continue
+
+        if in_atom:
+            line_split = line.split()
+            atmtypes.append(line_split[5])
+            charges.append(float(line_split[8]))
+            continue
+    if len(atmtypes) != natom:
+        sys.exit(f"Length of atom list ({len(atmtypes)}) and declared number of atoms ({natom}) differ!")
+    
+    return charges, atmtypes
+
+def get_mol2_out(orig, charges):
+    
+    in_atom = False
+    in_mol = False 
+    count = 0
+    out = ""
+
+    for line in orig:
+
+        if len(line.split()) == 0:
+            continue
+        if line.strip().startswith("#"):
+            continue
+
+        if "@<TRIPOS>MOLECULE" in line:
+            in_mol = True
+            count += 1
+            out += line
+            continue
+
+        if "@<TRIPOS>ATOM" in line:
+            in_atom = True
+            in_mol = False
+            atm_idx = 0
+            out += line 
+            continue
+
+        if "@<TRIPOS>BOND" in line:
+            in_atom = False
+
+        if "@<TRIPOS>ALT_TYPE" in line:
+            break
+
+        if in_mol:
+            count += 1
+            if count == 5:
+                out += "USER_CHARGES\n"
+                out += "Gaff 2 parametrization\n"
+                continue
+
+        if in_atom:
+            ls = line.split()
+            out += ("%7s %-8s%10.4f%10.4f%10.4f %-7s%5s %-11s%7.4f\n") % (int(ls[0]), ls[1], float(ls[2]), float(ls[3]), float(ls[4]), ls[5], "1", "LIG", float(charges[atm_idx]))
+            atm_idx += 1
+            continue
+
+        out += line
+    
+    return out
+
+def main(argv):
+    mol2in = argv[0]
+    gaffin = argv[1]
+
+    with open(mol2in, "r") as f:
+        orig = f.readlines()
+    with open(gaffin, "r") as f:
+        gaff = f.readlines()
+    
+    charges, atmtypes = get_gaff_params(gaff)
+
+    # i = 1
+    # for q, at in zip(charges, atmtypes):
+    #     print(i, q, at)
+    #     i += 1
+
+    out = get_mol2_out(orig, charges)
+
+    # Now we add the CGenFF alternative atom types
+    out += "@<TRIPOS>ALT_TYPE\nCGAFF_2_ALT_TYPE_SET\nGAFF_2 "
+    for i, at in enumerate(atmtypes):
+        out += str(i+1)+" "+at+" "
+    out += "\n"
+
+    mol2out = os.path.splitext(mol2in)[0] + "_par.mol2"
+    with open(mol2out, "w") as f:
+        f.write(out)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
